b8797099e2885101d02774afa6aa42acaaba7906
linsu <lsu25@uwo.ca>
2017-02-12 01:45:59 -0500

    Added Course class and made changes to Student and Instructor.

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by linsu on 2017/2/12.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
